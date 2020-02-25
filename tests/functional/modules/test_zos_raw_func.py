7ad78a65479b856a2ab2d992a35b69e21c7de971
“bjmaxy” <bjmaxy@cn.ibm.com>
2020-02-25 17:38:09 +0800

    zos-raw functional test

--- tests/functional/modules/test_zos_raw_func.py
+++ tests/functional/modules/test_zos_raw_func.py
@@ -13,51 +13,92 @@ import pytest
 
 __metaclass__ = type
 
-
+# Simple test without output.
 def test_zos_raw_IEFBR14(ansible_zos_module):
     hosts = ansible_zos_module
     results = hosts.all.zos_raw(program='IEFBR14')
     for result in results.contacted.values():
-        assert result.get('return_code') == '0'
+        assert result.get('ret_code').get('code') == 0
         assert result.get('changed') == True
 
-
- def test_zos_raw_iebgener(ansible_zos_module):
+#run program iebgener, copy the content from sysut1 to sysut2.
+def test_zos_raw_iebgener(ansible_zos_module):
     dds_test = [{'ddName': 'sysin', 'dataset': 'dummy'},
-            {'ddName': 'sysut1', 'dataset': 'imstestl.ims1.test05'},
-            {'ddName': 'sysut2', 'dataset': 'imstestl.ims1.test06'},
+            {'ddName': 'sysut1', 'dataset': 'IMSTESTL.IMS1.TEST05'},
+            {'ddName': 'sysut2', 'dataset': 'IMSTESTL.IMS1.TEST06'},
             {'ddName': 'sysprint', 'dataset': 'stdout'}
             ]
     hosts = ansible_zos_module
-    hosts.all.zos_dataset(name='imstestl.ims1.test05', state='present', type='seq', replace=True)
-    hosts.all.zos_dataset(name='imstestl.ims1.test06', state='present', type='seq', replace=True)
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST05', state='present', type='seq', replace=True)
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST06', state='present', type='seq', replace=True)
     results = hosts.all.zos_raw(program='iebgener', dds=dds_test, debug=True)
     for result in results.contacted.values():
-        assert result.get('return_code') == '0'
+        assert result.get('ret_code').get('code') == 0
         assert result.get('changed') == True
 
-def test_zos_raw_ISRSUPC(ansible_zos_module):
-        # Compare 2 PDS members olddd and newdd and write the output to outdd. 
-        # The content of newdd and olddd is not the same, so failed with rc 35.
-    dds_test = [{'ddName': 'newdd', 'dataset': 'BJMAXY.MVSUTIL.PYTHON.MVSCMD.A'},
-            {'ddName': 'olddd', 'dataset': 'BJMAXY.MVSUTIL.PYTHON.MVSCMD.B'},
-            {'ddName': 'sysin', 'dataset': 'BJMAXY.MVSUTIL.PYTHON.MVSCMD.OPT'},
-            {'ddName': 'outdd', 'dataset': 'BJMAXY.MVSUTIL.PYTHON.MVSCMD.RESULT'}
-            ]
-    hosts = ansible_zos_module
-    results = hosts.all.zos_raw(program='ISRSUPC', dds=dds_test, debug=True)
-    for result in results.contacted.values():
-        assert result.get('return_code') == '35'
+# def test_zos_raw_ISRSUPC(ansible_zos_module):
+#         # Compare 2 PDS members olddd and newdd and write the output to outdd. 
+#         # The content of newdd and olddd is the same, so rc=0.
+#         # The content of newdd and olddd can be just a simple string like "TEST".
+#         # If you want to play this case, fill in the newdd and olddd with the same string content.
+#     dds_test = [{'ddName': 'newdd', 'dataset': 'IMSTESTL.IMS1.TEST05'},
+#             {'ddName': 'olddd', 'dataset': 'IMSTESTL.IMS1.TEST06'},
+#             {'ddName': 'sysin', 'content': '   CMPCOLM 1:72'},
+#             {'ddName': 'outdd', 'dataset': 'IMSTESTL.IMS1.TEST07'}
+#             ]
+#     hosts = ansible_zos_module
+#     hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST05', state='present', type='seq', replace=True)
+#     hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST06', state='present', type='seq', replace=True)
+#     hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST07', state='present', type='seq', replace=True)
+#     results = hosts.all.zos_raw(program='ISRSUPC', dds=dds_test, debug=True)
+#     for result in results.contacted.values():
+#         assert result.get('ret_code').get('code') == 0
 
-def test_zos_raw_ISRSUPC_content(ansible_zos_module):
+def test_zos_raw_ISRSUPC_with_content(ansible_zos_module):
         # Compare 2 PDS members olddd and newdd and write the output to outdd. 
-        # The content of newdd and olddd is the same, so rc=0.
-    dds_test = [{'ddName': 'newdd', 'content': 'test'},
-            {'ddName': 'olddd', 'content': 'test'},
+        # The content of newdd and olddd is not the same, so rc=1.
+    dds_test = [{'ddName': 'newdd', 'content': 'TEST'},
+            {'ddName': 'olddd', 'content': 'TEST12'},
             {'ddName': 'sysin', 'content': '   CMPCOLM 1:72'},
-            {'ddName': 'outdd', 'dataset': 'BJMAXY.MVSUTIL.PYTHON.MVSCMD.RESULT'}
+            {'ddName': 'outdd', 'dataset': 'IMSTESTL.IMS1.TEST07'}
             ]
     hosts = ansible_zos_module
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST07', state='present', type='seq', replace=True)
     results = hosts.all.zos_raw(program='ISRSUPC', dds=dds_test, debug=True)
     for result in results.contacted.values():
-        assert result.get('return_code') == '0'
+        assert result.get('ret_code').get('code') == 1
+
+def test_zos_raw_IDCAMS(ansible_zos_module):
+        # run IDCAMS program: list catalog of dataset IMSTESTL.IMS1.TEST05
+    dds_test = [{'ddName': 'sysin', 'content': "LISTCAT ENT('IMSTESTL.IMS1.TEST05')"},
+            {'ddName': 'sysprint', 'dataset': 'IMSTESTL.IMS1.TEST08'},
+            ]
+    hosts = ansible_zos_module
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST05', state='present', type='seq', replace=True)
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST08', state='present', type='seq', replace=True)
+    results = hosts.all.zos_raw(program='IDCAMS', auth=True, dds=dds_test, debug=True)
+    for result in results.contacted.values():
+        assert result.get('ret_code').get('code') == 0
+
+def test_zos_raw_IDCAMS_failure(ansible_zos_module):
+        # run IDCAMS program: list catalog of dataset IMSTESTL.IMS1.TEST05.0000 which does not exist.
+        # return error code 12
+    dds_test = [{'ddName': 'sysin', 'content': "LISTCAT ENT('IMSTESTL.IMS1.TEST05.0000')"},
+            {'ddName': 'sysprint', 'dataset': 'IMSTESTL.IMS1.TEST08'},
+            ]
+    hosts = ansible_zos_module
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST08', state='present', type='seq', replace=True)
+    results = hosts.all.zos_raw(program='IDCAMS', auth=True, dds=dds_test, debug=True)
+    for result in results.contacted.values():
+        assert result.get('ret_code').get('code') == 12
+
+def test_zos_raw_IDCAMS_without_auth(ansible_zos_module):
+        # run IDCAMS program: list catalog of dataset IMSTESTL.IMS1.TEST05
+    dds_test = [{'ddName': 'sysin', 'content': "LISTCAT ENT('IMSTESTL.IMS1.TEST05')"},
+            {'ddName': 'sysprint', 'dataset': 'IMSTESTL.IMS1.TEST08'},
+            ]
+    hosts = ansible_zos_module
+    hosts.all.zos_data_set(name='IMSTESTL.IMS1.TEST08', state='present', type='seq', replace=True)
+    results = hosts.all.zos_raw(program='IDCAMS', dds=dds_test, debug=True)
+    for result in results.contacted.values():
+        assert result.get('ret_code').get('code') == 36
