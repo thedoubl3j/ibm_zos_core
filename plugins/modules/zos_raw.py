9ce1809edd5e8b145ecb47700ab9255ea5f80c44
“bjmaxy” <bjmaxy@cn.ibm.com>
2020-02-25 14:32:31 +0800

    zos-raw update

--- plugins/modules/zos_raw.py
+++ plugins/modules/zos_raw.py
@@ -22,6 +22,13 @@ options:
     type: str
     description:
       - The program arguments, e.g. -a='MARGINS(1,72)'. 
+  auth:
+    required: false
+    type: bool
+    default: false
+    description: 
+      - Instruct whether this program should run authorized or not. 
+      - If set to true, the program will be run as APF authorized, otherwise the program runs as unauthorized.
   dds:
     required: false 
     type: List
@@ -38,23 +45,54 @@ options:
 """
 
 RETURN = """
-return_code: 
+ret_code: 
     description: The return code. 
     returned : always
-    type: str
+    type: dict
+    contains:
+        msg:
+            description: Holds the return code 
+            type: str
+        msg_code: 
+            description: Holds the return code string 
+            type: str
+        msg_txt: 
+            description: Holds additional information related to the program that may be useful to the user.
+            type: str
+        code: 
+            description: return code converted to integer value (when possible)
+            type: int 
     sample: Value 0 indicates success, non-zero indicates failure.
-outdd:
-    description: The program output.
-    returned : Displayed if outdd is returned. 
-    type: str
-sysprint:
-    description: The sysprint message. 
-    returned : always
-    type: str
-stdout:
-    description: The standard IZOAU command line output. 
-    returned : always
-    type: str
+       - code: 0
+       - msg: "0"
+       - msg_code: "0"
+       - msg_txt: "THE z/OS PROGRAM EXECUTION SUCCEED." 
+ddnames:
+    description: All the related dds with the program. 
+    returned: always
+    type: list<dict> 
+    contains:
+        ddname:
+          description: data definition name
+          type: str
+        dataset:
+          description: the dataset name  
+          type: str
+        content:
+          description: ddname content
+          type: list[str] 
+        record_count:
+          description: the lines of the content 
+          type: int
+        byte_count:
+          description: bytes count
+          type: int
+    samples:         
+        - ddname: "SYSIN", "SYSPRINT",etc.
+        - dataset: "TEST.TESTER.DATA", "stdout", "dummy", etc
+        - content: " "
+        - record_count: 4
+        - byte_count:  415
 changed: 
     description: Indicates if any changes were made during module operation.
     type: bool
@@ -69,6 +107,7 @@ EXAMPLES = r"""
   zos_raw:
     program: IDCAMS
     parms:
+    auth: true
     dds: [{ddName: sysin, content: LISTCAT ENT('TESTER.DATASET')},
           {ddName: sysprint, dataset: TESTER.DATASET.RESULT}
          ] 
@@ -82,63 +121,146 @@ EXAMPLES = r"""
             ]
      debug: True
      
-- name: Run ISRSUPC program. Compare 2 PDS members olddd and newdd and write the output to outdd.   
+- name: Run isrsupc program,Compare 2 PDS members olddd and newdd and write the output to outdd. 
   zos_raw:
-     program: ISRSUPC
-     parms: DELTAL,LINECMP
-     dds: [{ddName: newdd, content: TEST},
-           {ddName: olddd, content: TEST},
-           {ddName: sysin, content:    CMPCOLM 1:72},
-           {ddName: outdd, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.RESULT}
-          ]
-     verbose: True
+    program: ISRSUPC
+    parms: DELTAL,LINECMP
+    dds: [{ddName: newdd, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.A},
+          {ddName: olddd, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.B},
+          {ddName: sysin, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.OPT},
+          {ddName: outdd, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.RESULT},
+          {ddName: sysprint, dataset: TESTER.MVSUTIL.PYTHON.MVSCMD.RESULT2}
+         ]
+
      
-Example results:
-"msg": " {'msg': 'THE z/OS PROGRAM EXECUTION SUCCEED.', 
-          'changed': True, 
-          'outdd': None, 
-          'sysprint': None, 
-          'return_code': '0', 
-          'stdout': '1DATA SET UTILITY - GENERATE PAGE 0001\\n-\\n PROCESSING ENDED AT EOD\\n0\\n', 
-          'stdout_lines': ['1DATA SET UTILITY - GENERATE PAGE 0001  ', '- ', ' PROCESSING ENDED AT EOD', '0'], 
-          'failed': False}" 
-          
- "msg": " {'msg': 'THE z/OS PROGRAM EXECUTION SUCCEED.', 
-           'changed': True, 
-           'outdd': None, 
-           'sysprint': \"1IDCAMS  SYSTEM SERVICES TIME: 08:40:45        02/06/20     PAGE 1\\n0\\n  
-                        LISTCAT ENT('TESTER.DATASET')\\n
-                        0NONVSAM ------- TESTER.DATASET\\n      IN-CAT --- ICFCAT.SYSPLEX2.CATALOGB\\n
-                        1IDCAMS  SYSTEM SERVICES TIME: 08:40:45        02/06/20     PAGE      2\\n0
-                        THE NUMBER OF ENTRIES PROCESSED WAS:\\n
-                          AIX -------------------0\\n
-                          ALIAS -----------------0\\n                    
-                          CLUSTER ---------------0\\n                    
-                          DATA ------------------0\\n                    
-                          GDG -------------------0\\n                    
-                          INDEX -----------------0\\n                    
-                          NONVSAM ---------------1\\n                    
-                          PAGESPACE -------------0\\n                    
-                          PATH ------------------0\\n                    
-                          SPACE -----------------0\\n                    
-                          USERCATALOG -----------0\\n                    
-                          TAPELIBRARY -----------0\\n                    
-                          TAPEVOLUME ------------0\\n                    
-                          TOTAL -----------------1\\n0         
-                        THE NUMBER OF PROTECTED ENTRIES SUPPRESSED WAS 0\\n0
-                        IDC0001I FUNCTION COMPLETED, HIGHEST CONDITION CODE WAS 0\\n0        \\n
-                        0IDC0002I IDCAMS PROCESSING COMPLETE. MAXIMUM CONDITION CODE WAS 0\", 
-           'return_code': '0', 
-           'stdout': '0\\n', 
-           'stdout_lines': ['0'], 
-           'failed': False}"
+EXAMPLE RESULTS:
+"msg": "    
+    "changed": true,
+    "ddnames": [
+        {
+            "byte_count": "748",
+            "content": "TEST                                                                            ",
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.A",
+            "ddname": "newdd",
+            "record_count": 1
+        },
+        {
+            "byte_count": "748",
+            "content": "TEST                                                                            ",
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.B",
+            "ddname": "olddd",
+            "record_count": 1
+        },
+        {
+            "byte_count": "748",
+            "content": "   CMPCOLM 1:72                                                                 ",
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.OPT",
+            "ddname": "sysin",
+            "record_count": 1
+        },
+        {
+            "byte_count": "3400",
+            "content": "
+                       ISRSUPC   -   MVS/PDF FILE/LINE/WORD/BYTE/SFOR COMPARE UTILITY- ISPF FOR z/OS         2020/02/25   3.36    PAGE     1
+                        NEW: TESTER.MVSUTIL.PYTHON.MVSCMD.A                          OLD: TESTER.MVSUTIL.PYTHON.MVSCMD.B                       
+                                                                                                                       
+                                            LINE COMPARE SUMMARY AND STATISTICS                                                             
+                                                                                                                       
+                                                                                                                       
+                                                                                                                       
+                            1 NUMBER OF LINE MATCHES               0  TOTAL CHANGES (PAIRED+NONPAIRED CHNG)                                 
+                            0 REFORMATTED LINES                    0  PAIRED CHANGES (REFM+PAIRED INS/DEL)                                  
+                            0 NEW FILE LINE INSERTIONS             0  NON-PAIRED INSERTS                                                    
+                            0 OLD FILE LINE DELETIONS              0  NON-PAIRED DELETES                                                    
+                            1 NEW FILE LINES PROCESSED                                                                                      
+                            1 OLD FILE LINES PROCESSED                                                                                      
+                                                                                                                       
+                    LISTING-TYPE = DELTA      COMPARE-COLUMNS =    1:72        LONGEST-LINE = 80                                           
+                    PROCESS OPTIONS USED: NONE                                                                                             
+                                                                                                                       
+                    THE FOLLOWING PROCESS STATEMENTS (USING COLUMNS 1:72) WERE PROCESSED:                                                  
+                            CMPCOLM 1:72                                                                                                     
+                     " 
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.RESULT",
+            "ddname": "outdd",
+            "record_count": 20
+        },
+        {
+            "byte_count": "0",
+            "content": "",
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.RESULT2",
+            "ddname": "sysprint",
+            "record_count": 1
+        }
+    ],
+    "ret_code": {
+        "code": 0,
+        "msg": 0,
+        "msg_code": 0,
+        "msg_txt": "THE z/OS PROGRAM EXECUTION SUCCEED.",
+    }
+"
+
+"msg" : "
+    "changed": true,
+    "ddnames": [
+        {
+            "byte_count": "748",
+            "content": "  LISTCAT ENT('TESTER.HILL3')                                                   ",
+            "dataset": "TESTER.P3621680.T0979893.C0000000",
+            "ddname": "sysin",
+            "record_count": 1
+        },
+        {
+            "byte_count": "2006",
+            "content": "
+                        1IDCAMS  SYSTEM SERVICES                                           TIME: 04:04:12        02/25/20     PAGE      1 
+                        0                                                                                                                 
+                        LISTCAT ENT('TESTER.HILL3')                                                                                    
+                        0NONVSAM ------- TESTER.HILL3                                                                                     
+                              IN-CAT --- ICFCAT.SYSPLEX2.CATALOGB                                                                         
+                        1IDCAMS  SYSTEM SERVICES                                           TIME: 04:04:12        02/25/20     PAGE      2 
+                        0         THE NUMBER OF ENTRIES PROCESSED WAS:                                                                    
+                                            AIX -------------------0                                                                      
+                                            ALIAS -----------------0                                                                      
+                                            CLUSTER ---------------0                                                                      
+                                            DATA ------------------0                                                                      
+                                            GDG -------------------0                                                                      
+                                            INDEX -----------------0                                                                      
+                                            NONVSAM ---------------1                                                                      
+                                            PAGESPACE -------------0                                                                      
+                                            PATH ------------------0                                                                      
+                                            SPACE -----------------0                                                                      
+                                            USERCATALOG -----------0                                                                      
+                                            TAPELIBRARY -----------0                                                                      
+                                            TAPEVOLUME ------------0                                                                      
+                                            TOTAL -----------------1                                                                      
+                        0         THE NUMBER OF PROTECTED ENTRIES SUPPRESSED WAS 0                                                        
+                        0IDC0001I FUNCTION COMPLETED, HIGHEST CONDITION CODE WAS 0                                                        
+                        0                                                                                                                 
+                        0IDC0002I IDCAMS PROCESSING COMPLETE. MAXIMUM CONDITION CODE WAS 0                                                
+            " 
+            "dataset": "TESTER.MVSUTIL.PYTHON.MVSCMD.AUTH.OUT",
+            "ddname": "sysprint",
+            "record_count": 25
+        }
+    ], 
+    "ret_code": {
+        "code": 0,
+        "msg": 0,
+        "msg_code": 0,
+        "msg_txt": "THE z/OS PROGRAM EXECUTION SUCCEED."
+    }
+
+
+"
+
 
 """
 
 from ansible.module_utils.basic import *
 from zoautil_py import MVSCmd, Datasets
-from os import chmod, path, remove
-from tempfile import NamedTemporaryFile
+import re
 
 MVSCMD = "mvscmd"
 MVSCMD_AUTH = "mvscmdauth"
@@ -177,10 +299,48 @@ def delete_data_sets(data_sets):
     for data_set in data_sets:
         Datasets.delete(data_set)
 
+def parse_dds(dds,stdout):
+    ddnames = []
+    for item in dds:
+        if item['dataset'] == 'stdout':
+            content = stdout
+            ddname = {
+                'ddname': item['ddName'],
+                'dataset': item['dataset'],
+                'content': content,
+                'record_count': len(re.split(r'\n*', content)),  # the lines of the content
+                'byte_count': content.__sizeof__(),  # the bytes of the content
+            }
+        elif item['dataset'] == 'dummy':
+            content = ""
+            ddname = {
+                'ddname': item['ddName'],
+                'dataset': item['dataset'],
+                'content': content,
+                'record_count': '0',
+                'byte_count': '0',
+            }
+        else:
+            content = Datasets.read(item['dataset'])
+            dataset_info = Datasets.list(item['dataset'], verbose=True)
+            if content == None:
+                content = ""
+            ddname = {
+                'ddname': item['ddName'],
+                'dataset': item['dataset'],
+                'content': content,
+                'record_count': len(re.split(r'\n', content)),
+                'byte_count': re.split(r'[\s]\s*', dataset_info)[6]  # In the position 6, it's used bytes.
+            }
+
+        ddnames.append(ddname)
+    return ddnames
+
+
 def run_module():
     module_args = dict(
         program=dict(type='str', required=True),
-        auth=dict(type='bool', required=True, default='False'),
+        auth=dict(type='bool', required=False, default=False),
         parms=dict(type='str', required=False),
         dds=dict(type='list', required=False),
         verbose=dict(type='bool', required=False),
@@ -193,10 +353,8 @@ def run_module():
     )
     result = dict(
         changed=False,
-        outdd='',
-        sysprint='',
-        return_code='',
-        stdout=''
+        ddnames='',
+        ret_code='',
     )
     hlq = Datasets.hlq()
     temp_dds = []
@@ -206,17 +364,12 @@ def run_module():
 
     try:
         dds = module.params.get("dds")
-        sysprint_DS = ''
-        outdd = ''
         contain_sysprint = False
 
         if dds != None:
             for item in dds:
                 if item.get('ddName') == 'sysprint':
                     contain_sysprint = True
-                    sysprint_DS = item.get("dataset")
-                if item.get('ddName') == 'outdd':
-                    outdd = item.get('dataset')
                 if 'content' in item.keys():
                     TEMP_SYSIN_DS = Datasets.temp_name(hlq)
                     if Datasets.exists(TEMP_SYSIN_DS):
@@ -246,22 +399,35 @@ def run_module():
                                              dds,
                                              module.params.get("verbose"),
                                              module.params.get("debug"), module)
+        ret_code = {
+            'msg': rc,
+            'msg_code': rc,
+            'msg_txt': "",
+            'code': rc,
+        }
+        if rc == 0 :
+            ret_code['msg_txt'] = 'THE z/OS PROGRAM EXECUTION SUCCEED.'
+        if rc == 36:
+            ret_code['msg_txt'] = "BGYSC0236E Unable to call authorized program "+module.params.get("program")+". Please specify option 'auth' to true."
+        elif rc != 0:
+            ret_code['msg_txt'] = "THE z/OS PROGRAM EXECUTION FAILED."
 
-        result['outdd'] = Datasets.read(outdd)
-        result['sysprint'] = Datasets.read(sysprint_DS)
-        result['stdout'] = stdout
-        result['return_code'] = rc
+        ddnames = parse_dds(dds,stdout)
+
+        result['ddnames'] = ddnames
+        result['ret_code'] = ret_code
 
         if rc != 0:
-            module.fail_json(msg="THE z/OS PROGRAM EXECUTION FAILED.", **result)
+            module.fail_json(msg="THE z/OS program execution failed, please check the rec_code.",**result)
     except ZOSRawError as e:
-        module.fail_json(msg=e, **result)
+        module.fail_json(msg="ZOSRawError :"+str(e), **result)
     except Exception as e:
-        module.fail_json(msg=e, **result)
+        module.fail_json(msg="Exception :"+str(e), **result)
     finally:
         delete_data_sets(temp_dds)
+
     result['changed'] = True
-    module.exit_json(msg='THE z/OS PROGRAM EXECUTION SUCCEED.', **result)
+    module.exit_json(**result)
 
 class Error(Exception):
     pass
